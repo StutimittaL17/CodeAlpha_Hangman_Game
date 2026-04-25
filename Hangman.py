b494aa09e1bea1fd38565fb8a25a1acb2c3ea534
StutimittaL17 <stuti.mittal_cs.aiml24@gla.ac.in>
2026-04-25 23:31:23 +0530

    Add files via upload

---
+++ Hangman.py
@@ -0,0 +1,39 @@
+import random
+
+words = ["apple", "banana", "grapes", "mango", "peach"]
+word = random.choice(words)
+
+guessed = []
+attempts = 6
+
+print("🎮 Welcome to Hangman!")
+
+while attempts > 0:
+    display = ""
+
+    for letter in word:
+        if letter in guessed:
+            display += letter + " "
+        else:
+            display += "_ "
+
+    print("\nWord:", display)
+
+    if "_" not in display:
+        print("🎉 You won!")
+        break
+
+    guess = input("Enter a letter: ").lower()
+
+    if guess in guessed:
+        print("⚠ Already guessed!")
+        continue
+
+    guessed.append(guess)
+
+    if guess not in word:
+        attempts -= 1
+        print("❌ Wrong! Attempts left:", attempts)
+
+if attempts == 0:
+    print("💀 Game Over! The word was:", word)
